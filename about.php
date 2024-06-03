4d52dec7a960d6832e16d3d3d4fba436b495bb66
Tombontsoa <printom2@gmail.com>
2024-06-03 18:33:15 +0300

    first commit

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>About</title>
+    </head>
+    <body>
+        <nav>
+            <a href="home.php" > Home </a>
+            <a href="about.php" > About </a>
+        </nav>
+        <h1> About </h1>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>My First App</title>
+    </head>
+    <body>
+        <nav>
+            <a href="home.php" > Home </a>
+            <a href="about.php" > About </a>
+        </nav>
+        <h1> Home </h1>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
